dec5b8f02ab0e0100ee6e87e84c7d8a96716a75d
akai777 <akai777@gmail.com>
2013-05-08 10:39:15 +0000

    Update Source Code, new Test Page

---
+++ SourceCode/OATS_Capstone/OATS_Capstone/Controllers/TestsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OATS_Capstone.Controllers
+{
+    public class TestsController : Controller
+    {
+        //
+        // GET: /Tests/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewTest()
+        {
+            return View();
+        }
+
+    }
+}
