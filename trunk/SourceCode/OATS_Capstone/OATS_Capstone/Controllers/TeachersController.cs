75f1af6df2d9d0c6e8ae821cdd09c36b48ccadef
akai777@gmail.com <akai777@gmail.com@3859dce6-03bb-3f17-ec6e-671fde64d787>
2013-05-08 10:39:15 +0000

    Update Source Code, new Test Page

---
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Controllers/TeachersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OATS_Capstone.Controllers
+{
+    public class TeachersController : Controller
+    {
+        //
+        // GET: /Teachers/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
