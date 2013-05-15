fec95fa78a2b5048512495b07cd741df2503d332
akai777@gmail.com <akai777@gmail.com@3859dce6-03bb-3f17-ec6e-671fde64d787>
2013-05-15 18:07:46 +0000

    Update source

---
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Models/SingletonDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OATS_Capstone.Models
+{
+    public class SingletonDb
+    {
+        private static OATSDBEntities _oatsEntities= null;
+        public static OATSDBEntities Instance()
+        {
+            if (_oatsEntities == null)
+            {
+                _oatsEntities = new OATSDBEntities();
+            }
+            return _oatsEntities;
+        }
+    }
+}
