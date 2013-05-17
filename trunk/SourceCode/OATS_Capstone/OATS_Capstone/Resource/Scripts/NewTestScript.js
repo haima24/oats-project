f48e6858bd5ce2e1b72ec014093ebb8c17f86b15
akai777@gmail.com <akai777@gmail.com@3859dce6-03bb-3f17-ec6e-671fde64d787>
2013-05-17 10:19:21 +0000

    Update source

--- trunk/SourceCode/OATS_Capstone/OATS_Capstone/Resource/Scripts/NewTestScript.js
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Resource/Scripts/NewTestScript.js
@@ -1,5 +1,8 @@
 ﻿var questions;
+var emptylist;
 $(function () {
+    emptylist = $(".nt-empty-list-ph");
+        //separator
     $(".tab-event").live("click", function (e) {
         e.preventDefault();
         var link = e.target;
@@ -13,6 +16,9 @@ $(function () {
             var tabcontent = $("#eventTab");
             if (tabcontent && res.tab) {
                 tabcontent.html(res.tab);
+
+                //rebind accordition
+                $("#sidebar").accordion();
             }
         });
     });
@@ -27,12 +33,43 @@ $(function () {
         var cur = $(ev.currentTarget);
         var etab = $("#checklist");
         if (questions && etab) {
-            if (cur.hasClass("t-question-type-radio")) { etab.append(questions.radio);}
-            if (cur.hasClass("t-question-type-multiple")) { etab.append(questions.multiple); }
-            if (cur.hasClass("t-question-type-essay")) { etab.append(questions.essay); }
-            if (cur.hasClass("t-question-type-short")) { etab.append(questions.shortanswer); }
-            if (cur.hasClass("t-question-type-text")) { etab.append(questions.text); }
-            if (cur.hasClass("t-question-type-img")) { etab.append(questions.image); }
+            if (cur.hasClass("t-question-type-radio")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.radio) : etab.append(questions.radio); }
+            if (cur.hasClass("t-question-type-multiple")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.multiple) : etab.append(questions.multiple); }
+            if (cur.hasClass("t-question-type-essay")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.essay) : etab.append(questions.essay); }
+            if (cur.hasClass("t-question-type-short")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.shortanswer) : etab.append(questions.shortanswer); }
+            if (cur.hasClass("t-question-type-text")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.text) : etab.append(questions.text); }
+            if (cur.hasClass("t-question-type-img")) { $(".nt-empty-list-ph", etab).length == 1 ? etab.html(questions.image) : etab.append(questions.image); }
         }
     });
+    //separator
+    $(".bt-duplicate").live("click", function (ev) {
+        var content = $(ev.target).closest(".nt-qitem.nt-qitem-edit");
+        var etab = $("#checklist");
+        if (content && etab) {
+            etab.append(content.prop("outerHTML"))
+        }
+    });
+    //separator
+    $(".bt-delete").live("click", function (ev) {
+        var content = $(ev.target).closest(".nt-qitem.nt-qitem-edit");
+        content.remove()
+        if ($("#checklist").children().length == 0) {
+            $("#checklist").html(emptylist);
+        }
+    });
+    //separator
+    $(".t-question-type").draggable({
+        connectToSortable: "#checklist",
+        helper: "clone",
+        revert: "invalid"
+    });
+    $("#checklist").sortable({
+        revert: true,
+        tolerance: 'pointer',
+        revert: 50,
+        distance: 5,
+        items: '>.nt-qitem',
+        placeholder: 'highlight'
+    });
+    //separator
 });
--- trunk/SourceCode/OATS_Capstone/OATS_Capstone/Resource/Scripts/Tests_Layout.js
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Resource/Scripts/Tests_Layout.js
@@ -20,26 +20,28 @@
         all.find('button.nt-btn-sqr.nt-qctrls-qtags-toggle').hide();
     });
     $(".nt-qitem").live('click', function (ev) {
-        var all = $(".nt-qitem");
-        all.removeClass('nt-qitem-edit-act');
-        all.removeClass('nt-qitem-edit-inact');
-        $(this).addClass('nt-qitem-edit-act').siblings().addClass('nt-qitem-edit-inact');
-        var current = ev.target;
-        var pr = $(current).closest('.nt-qitem');
-        if (current.parentNode && $(current.parentNode).hasClass('nt-tag-adder')) {
-            var btn = pr.find('button.nt-btn-sqr.nt-qctrls-qtags-toggle')[0];
-            $(pr.find('.nt-qctrls')[0]).hide();
-            $(pr.find('.nt-qtagcont')[0]).show();
-            $(btn).find('i').removeClass('i-tag').addClass('i-setting');
-            $(btn).show();
-        } else {
-            var btn = pr.find('button.nt-btn-sqr.nt-qctrls-qtags-toggle')[0];
-            $(pr.find('.nt-qctrls')[0]).show();
-            $(pr.find('.nt-tag-adder')[0]).hide();
-            $(btn).find('i').removeClass('i-setting').addClass('i-tag');
-            $(btn).show();
-            var btnAdd = pr.find('button.nt-btn-text.nt-qansadd')[0];
-            $(btnAdd).show();
+        if($(this).parent().length!=0){
+            var all = $(".nt-qitem");
+            all.removeClass('nt-qitem-edit-act');
+            all.removeClass('nt-qitem-edit-inact');
+            $(this).addClass('nt-qitem-edit-act').siblings().addClass('nt-qitem-edit-inact');
+            var current = ev.target;
+            var pr = $(current).closest('.nt-qitem');
+            if (current.parentNode && $(current.parentNode).hasClass('nt-tag-adder')) {
+                var btn = pr.find('button.nt-btn-sqr.nt-qctrls-qtags-toggle')[0];
+                $(pr.find('.nt-qctrls')[0]).hide();
+                $(pr.find('.nt-qtagcont')[0]).show();
+                $(btn).find('i').removeClass('i-tag').addClass('i-setting');
+                $(btn).show();
+            } else {
+                var btn = pr.find('button.nt-btn-sqr.nt-qctrls-qtags-toggle')[0];
+                $(pr.find('.nt-qctrls')[0]).show();
+                $(pr.find('.nt-tag-adder')[0]).hide();
+                $(btn).find('i').removeClass('i-setting').addClass('i-tag');
+                $(btn).show();
+                var btnAdd = pr.find('button.nt-btn-text.nt-qansadd')[0];
+                $(btnAdd).show();
+            }
         }
     });
 });
