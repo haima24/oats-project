71ceaeaf980b0212b22efc6aef43987451d5c5a5
akai777@gmail.com <akai777@gmail.com@3859dce6-03bb-3f17-ec6e-671fde64d787>
2013-05-17 15:13:10 +0000

    Search function demo

---
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Models/SearchingTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OATS_Capstone.Models
+{
+    public class SearchingTests
+    {
+        public int Id;
+        public string TestTitle;
+        public DateTime StartDate;
+        public DateTime? EndDate;
+    }
+}
