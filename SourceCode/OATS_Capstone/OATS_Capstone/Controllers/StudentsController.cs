8c76632db4da855018b55a4dfe7224ae1ed1b186
iamanhan722 <iamanhan722@gmail.com>
2013-05-20 06:14:03 +0000

    search

--- SourceCode/OATS_Capstone/OATS_Capstone/Controllers/StudentsController.cs
+++ SourceCode/OATS_Capstone/OATS_Capstone/Controllers/StudentsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OATS_Capstone.Models;
+using TugberkUg.MVC.Helpers;
 
 namespace OATS_Capstone.Controllers
 {
@@ -10,6 +12,22 @@ namespace OATS_Capstone.Controllers
     {
         //
         // GET: /Students/
+
+        public JsonResult StudentsSearch()
+        {
+            var db = SingletonDb.Instance();
+            var students = db.Users.Where(i => i.Role.RoleID == 2).ToList();
+            var listStudentsSearch = new List<SearchingStudents>();
+            students.ForEach(delegate(User student ){
+            var studentTemplate =new SearchingStudents();
+            studentTemplate.UserID = student.UserID;
+            studentTemplate.RoleID = student.RoleID;
+            studentTemplate.LastName = student.LastName;
+            studentTemplate.FirstName = student.FirstName;
+            listStudentsSearch.Add(studentTemplate);
+            });
+            return Json(listStudentsSearch, JsonRequestBehavior.DenyGet);
+        }
 
         public ActionResult Index()
         {
--- SourceCode/OATS_Capstone/OATS_Capstone/Controllers/TeachersController.cs
+++ SourceCode/OATS_Capstone/OATS_Capstone/Controllers/TeachersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OATS_Capstone.Models;
 
 namespace OATS_Capstone.Controllers
 {
@@ -10,6 +11,23 @@ namespace OATS_Capstone.Controllers
     {
         //
         // GET: /Teachers/
+
+        public JsonResult TeachersSearch()
+        {
+            var db = SingletonDb.Instance();
+            var teachers = db.Users.Where(i => i.Role.RoleID == 3).ToList();
+            var listTeachersSearch = new List<SearchingTeachers>();
+            teachers.ForEach(delegate(User teacher)
+            {
+                var teacherTemplate = new SearchingTeachers();
+                teacherTemplate.UserID = teacher.UserID;
+                teacherTemplate.RoleID = teacher.RoleID;
+                teacherTemplate.LastName = teacher.LastName;
+                teacherTemplate.FirstName = teacher.FirstName;
+                listTeachersSearch.Add(teacherTemplate);
+            });
+            return Json(listTeachersSearch, JsonRequestBehavior.DenyGet);
+        }
 
         public ActionResult Index()
         {
