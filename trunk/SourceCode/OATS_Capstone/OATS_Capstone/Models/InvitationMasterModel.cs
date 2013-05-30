405a09ad376b5e9468a91b0a49ceb581f15af6ec
akai777@gmail.com <akai777@gmail.com@3859dce6-03bb-3f17-ec6e-671fde64d787>
2013-05-30 09:31:10 +0000

    Done IMPORT QUESTION function

---
+++ trunk/SourceCode/OATS_Capstone/OATS_Capstone/Models/InvitationMasterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OATS_Capstone.Models
+{
+    public class InvitationMasterModel
+    {
+        public List<Invitation> InvitationList { get; set; }
+        public List<User> UserList { get; set; }
+    }
+}
