76b8d81c9c54c3f50df376d02320f64ea0068595
akai777 <akai777@gmail.com>
2013-07-01 13:46:07 +0000

    Update source, done Score

--- SourceCode/OATS_Capstone/OATS_Capstone/Models/TestScoreLogic.cs
+++ SourceCode/OATS_Capstone/OATS_Capstone/Models/TestScoreLogic.cs
@@ -13,9 +13,10 @@ namespace OATS_Capstone.Models
         public int CheckedUserIdsCount { get { return CheckedUserIds.Count; } }
         public string ScoreUsersNameLabel { get; set; }
         public List<ScoreTag> AvailableTags { get; set; }
+        public ScoreStatistics Overall { get; set; }
         public ScoreTest(Test test, List<int> checkIds)
         {
-            var db=SingletonDb.Instance();
+            var db = SingletonDb.Instance();
             CheckedUserIds = checkIds;
             TotalScoreOfTest = test.Questions.Sum(i =>
             {
@@ -34,30 +35,153 @@ namespace OATS_Capstone.Models
                     ScoreUserList.Add(item);
                 }
             });
-          
-            var tags = test.Questions.SelectMany(k => k.TagInQuestions.Select(i => i.Tag)).ToList();
-            AvailableTags = new List<ScoreTag>();
-            tags.ForEach(i =>
-                {
-                    if (i != null)
-                    {
-                        var scoreTag = new ScoreTag(i,test.TestID,checkIds);
-                        AvailableTags.Add(scoreTag);
-                    }
-                });
             if (CheckedUserIds.Count != 0)
             {
-                var names = ScoreUserList.Where(t=>CheckedUserIds.Contains(t.UserID)).Select(k => k.UserLabel).Aggregate((i, o) => i + "," + o);
+                var names = ScoreUserList.Where(t => CheckedUserIds.Contains(t.UserID)).Select(k => k.UserLabel).Aggregate((i, o) => i + "," + o);
                 if (names != null) { ScoreUsersNameLabel = names; }
+                var tags = test.Questions.SelectMany(k => k.TagInQuestions.Select(i => i.Tag)).ToList();
+                var groupTags = from tag in tags
+                                group tag by tag into GroupTags
+                                select new ScoreTag(GroupTags.Key, test.TestID, checkIds);
+                AvailableTags = groupTags.ToList();
+                Overall = new ScoreStatistics();
+                Overall.Name = "Overall";
+                var totalScore = test.Questions.TotalScore();
+                var groupScoreList = test.UserInTests.Select(i =>
+                {
+                    return i.UserInTestDetails.Sum(k => k.NonChoiceScore ?? 0 + k.ChoiceScore ?? 0);
+                });
+                var stdevScore = groupScoreList.StandardDeviation();
+                var averageScore = groupScoreList.Average();
+                var userScoreList = test.UserInTests.Where(k => checkIds.Contains(k.UserID)).Select(i =>
+                {
+                    return i.UserInTestDetails.Sum(k => k.NonChoiceScore ?? 0 + k.ChoiceScore ?? 0);
+                });
+                var selectionAverageScore = userScoreList.Average();
+
+                Overall.SelectionAVGScore = new ScoreStatistic(selectionAverageScore, totalScore);
+                Overall.GroupAVGScore = new ScoreStatistic(averageScore, totalScore);
+                Overall.DifferenceScore = new ScoreStatistic(selectionAverageScore - averageScore, 1);
+                Overall.GroupSTDEVScore = new ScoreStatistic(stdevScore, 1);
+                Overall.ScoreList = userScoreList.ToList();
             }
         }
     }
     public class ScoreTag
     {
         public Tag Tag { get; set; }
-        public ScoreTag(Tag tag,int testid,List<int> checkIds)
+        private ScoreStatistics statistic = null;
+        public ScoreStatistics Statistic
+        {
+            get { return statistic; }
+        }
+        public ScoreTag(Tag tag, int testid, List<int> checkIds)
         {
             Tag = tag;
+            if (tag != null)
+            {
+                var questions = tag.TagInQuestions.Where(i => i.Question.TestID == testid).Select(k => k.Question);
+                statistic = new ScoreStatistics();
+                var totalScore = questions.TotalScore();
+                var groupScoreList = questions.Select(i =>
+                {
+                    return i.UserInTestDetails.Sum(k => k.NonChoiceScore ?? 0 + k.ChoiceScore ?? 0);
+                });
+                var stdevScore = groupScoreList.StandardDeviation();
+                var averageScore = groupScoreList.Average();
+                var userScoreList = questions.Select(i =>
+                {
+                    return i.UserInTestDetails.Sum(k =>
+                    {
+                        if (checkIds.Contains(k.UserInTest.UserID))
+                        {
+                            return k.NonChoiceScore ?? 0 + k.ChoiceScore ?? 0;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    });
+                });
+                var selectionAverageScore = userScoreList.Average();
+                statistic.Name = tag.TagName;
+                statistic.SelectionAVGScore = new ScoreStatistic(selectionAverageScore, totalScore);
+                statistic.GroupAVGScore = new ScoreStatistic(averageScore, totalScore);
+                statistic.DifferenceScore = new ScoreStatistic(selectionAverageScore - averageScore, 1);
+                statistic.GroupSTDEVScore = new ScoreStatistic(stdevScore, 1);
+                statistic.ScoreList = userScoreList.ToList();
+            }
+        }
+    }
+    public class ScoreStatistics
+    {
+        public string Name { get; set; }
+        public ScoreStatistic SelectionAVGScore { get; set; }
+        public ScoreStatistic GroupAVGScore { get; set; }
+        public ScoreStatistic DifferenceScore { get; set; }
+        public ScoreStatistic GroupSTDEVScore { get; set; }
+        public List<decimal> ScoreList { get; set; }
+        public String ScoreListString
+        {
+            get
+            {
+                var s = string.Empty;
+                try
+                {
+                    s = ScoreList.Select(i => i.ToString()).Aggregate((f, o) => f + "," + o);
+                }
+                catch (Exception)
+                {
+                    s = string.Empty;
+                }
+                return s;
+            }
+        }
+    }
+    public class ScoreStatistic
+    {
+        private decimal? numerator = 0;
+
+        public decimal? Numerator
+        {
+            get { return numerator; }
+        }
+        private decimal? denominator = 0;
+
+        public decimal? Denominator
+        {
+            get { return denominator; }
+        }
+
+        public ScoreStatistic(decimal? num, decimal? denom)
+        {
+            this.numerator = num;
+            this.denominator = denom;
+        }
+        public string Praction { get { return string.Format("{0:##0.##}", numerator) + "/" + string.Format("{0:##0.##}", denominator); } }
+        public string Percent
+        {
+            get
+            {
+                var s = "0";
+                if (denominator != 0)
+                {
+                    s = (numerator / denominator).ToPercent();
+                }
+                return s;
+            }
+        }
+        public string Decimal
+        {
+            get
+            {
+                var s = "0";
+                if (denominator != 0)
+                {
+                    s = string.Format("{0:##0.##}", (numerator / denominator));
+                }
+                return s;
+            }
         }
     }
     public class ScoreUserItem
