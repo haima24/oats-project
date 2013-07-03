1c3cc618e6564ac5dfbb118ff8da83e444b20f38
akai777 <akai777@gmail.com>
2013-07-03 09:52:45 +0000

    Update Source, Class diagram and more

---
+++ SourceCode/OATS_Capstone/OATS_Capstone/Models/TestListLogic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OATS_Capstone.Models
+{
+    public class TestList
+    {
+        private List<TestListItem> recentTests;
+
+        public List<TestListItem> RecentTests
+        {
+            get { return recentTests; }
+        }
+        private List<TestListItem> runningTests;
+
+        public List<TestListItem> RunningTests
+        {
+            get { return runningTests; }
+        }
+
+        private List<TestListItem> upComingTests;
+
+        public List<TestListItem> UpComingTests
+        {
+            get { return upComingTests; }
+        }
+
+        public TestList(IEnumerable<Test> tests)
+        {
+            runningTests = tests.Where(i => i.UserInTests.Count == 0).Select(k => {
+                var testListItem = new TestListItem(k);
+                return testListItem;
+            }).ToList();
+            recentTests = tests.Where(i => i.UserInTests.Count > 0).Select(k =>
+            {
+                var testListItem = new TestListItem(k);
+                return testListItem;
+            }).ToList();
+        }
+    }
+    public class TestListItem
+    {
+        public string TestTitle { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        private int allInvitedCount = 0;
+        private int resultedOnInvitedCount = 0;
+
+        public int ResultedOnInvitedCount
+        {
+            get { return resultedOnInvitedCount; }
+        }
+
+        public int AllInvitedCount
+        {
+            get { return allInvitedCount; }
+        }
+        public decimal? Average { get; }
+        public decimal? STDEV { get; }
+        public TestListItem(Test test)
+        {
+            TestTitle = test.TestTitle;
+            Start = test.StartDateTime;
+            End = test.EndDateTime;
+
+            var groupScoreList = test.UserInTests.Select(i =>
+            {
+                return i.UserInTestDetails.Sum(k => k.NonChoiceScore ?? 0 + k.ChoiceScore ?? 0);
+            });
+            var stdevScore = groupScoreList.StandardDeviation();
+            var averageScore = groupScoreList.Average();
+            var allInvited = test.Invitations.Where(i => i.Role.RoleDescription == "Student");
+            allInvitedCount = allInvited.Count();
+            resultedOnInvitedCount = allInvited.Count(i =>
+            {
+                var userInTest = test.UserInTests.FirstOrDefault(k => k.UserID == i.UserID);
+                return userInTest != null;
+            });
+        }
+    }
+}
